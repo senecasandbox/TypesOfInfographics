b18eda9b0b92fb896e7b8a67af34a4f3b10c8fb5
senecasandbox <58563392+senecasandbox@users.noreply.github.com>
2021-07-27 11:47:50 -0400

    draft1

---
+++ story_content/user.js
@@ -0,0 +1,79 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "64XGgquWAW9":
+        Script1();
+        break;
+      case "6AtQKcEbYDP":
+        Script2();
+        break;
+      case "6AyzT9JVUmA":
+        Script3();
+        break;
+      case "6IpklKCNb2E":
+        Script4();
+        break;
+      case "5mEuj8LL1uK":
+        Script5();
+        break;
+      case "5scvWS26Q5X":
+        Script6();
+        break;
+      case "6W5izd0namQ":
+        Script7();
+        break;
+      case "5l4jfUkgJC2":
+        Script8();
+        break;
+      case "5xU78oyi4ui":
+        Script9();
+        break;
+  }
+}
+
+function Script1()
+{
+  ga('send', 'screenview', {screenName: 'Welcome'});
+}
+
+function Script2()
+{
+  ga('send', 'event', 'Button', 'click', 'List Button');
+}
+
+function Script3()
+{
+  ga('send', 'event', 'Button', 'click', Comparison Button');
+}
+
+function Script4()
+{
+  ga('send', 'event', 'Button', 'click', Flow Button');
+}
+
+function Script5()
+{
+  ga('send', 'event', 'Button', 'click', Data Vis Button');
+}
+
+function Script6()
+{
+  ga('send', 'event', 'Button', 'click', Map Button');
+}
+
+function Script7()
+{
+  ga('send', 'event', 'Button', 'click', Timeline Button');
+}
+
+function Script8()
+{
+  ga('send', 'event', 'Button', 'click', Article Button');
+}
+
+function Script9()
+{
+  ga('send', 'event', 'Button', 'click', Source Button');
+}
+
